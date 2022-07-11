efa1318d561854febac1160963a9272210bfea0d
Agustin Schulze <agsp3rs3o@gmail.com>
2022-07-11 12:45:11 -0300

    Adding progress.js

---
+++ js/progress.js
@@ -0,0 +1,10 @@
+const circle = document.querySelector(".progress-ring__circle");
+const radius = circle;
+
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
+}
